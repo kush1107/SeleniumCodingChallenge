e26934e69f84310e00d4a8309253d55ebf6ec19e
Kushal Parikh <kushal.parikh@i3verticals.com>
2024-03-29 16:37:19 +0530

    Day1-19 Coding Answers

--- src/test/java/Day20.java
+++ src/test/java/Day20.java
@@ -1,8 +1,21 @@
+import com.google.zxing.*;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.HybridBinarizer;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 
 public class Day20 {
@@ -15,11 +28,46 @@ public class Day20 {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(25));
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
         driver.manage().window().maximize();
-        driver.get("https://qaplayground.dev/apps/covered/#");
+        driver.get("https://qaplayground.dev/apps/qr-code-generator/");
     }
     @AfterClass
     public void tearDown()
     {
         driver.quit();
+    }
+
+    @Test
+    public void ReadTextFrom_QRCodeTest() {
+
+        WebElement QR_InputField = new WebDriverWait(driver,Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@placeholder='Enter text or URL']")));
+        QR_InputField.sendKeys("I am a Automation QA Engineer");
+
+        WebElement generateQAButton = driver.findElement(By.xpath("//button[normalize-space()='Generate QR Code']"));
+        generateQAButton.click();
+
+        WebElement QR_generated = new WebDriverWait(driver,Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(By.xpath("//img[@alt='qr-code']")));
+        String qrCodeURL = QR_generated.getAttribute("src");
+        System.out.println(qrCodeURL);
+        URL url= null;
+        try {
+            url = new URL(qrCodeURL);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        BufferedImage bufferedimage= null;
+        try {
+            bufferedimage = ImageIO.read(url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        LuminanceSource luminanceSource=new BufferedImageLuminanceSource(bufferedimage);
+        BinaryBitmap binaryBitmap=new BinaryBitmap(new HybridBinarizer(luminanceSource));
+        Result result = null;
+        try {
+            result = new MultiFormatReader().decode(binaryBitmap);
+        } catch (NotFoundException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Text Fetched from QR Code is: "+result.getText());
     }
 }
